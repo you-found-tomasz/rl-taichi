2da326635a803c330f1651056807d1d5929f1ef8
aquamin9 <aquamin9@googlemail.com>
2022-03-01 16:21:43 -0800

    updating mesh creation

--- mesh_creation.py
+++ mesh_creation.py
@@ -1,9 +1,15 @@
-import dmsh
+#import dmsh
 import meshio
-import optimesh
+#import optimesh
 import numpy as np
+import numpy as np
+import pygalmesh
+
+s = pygalmesh.Ball([2.5, 4, 2.5], 1.0)
+mesh = pygalmesh.generate_mesh(s, max_cell_circumradius=0.2)
 
 
+'''
 geo = dmsh.Circle([2.5, 2.5], 1.0)
 X, cells = dmsh.generate(geo, 0.5)
 
@@ -12,18 +18,19 @@ X, cells = optimesh.optimize_points_cells(X, cells, "CVT (full)", 1.0e-10, 100)
 
 # visualize the mesh
 dmsh.helpers.show(X, cells, geo)
+'''
 
-points = X
+points = mesh
 
 
-x = X[:,0]
-y = np.ones(len(x))*0.5
-z = X[:,1]
-elements = cells
-num_tris = len(elements)
+x = mesh.points[:,0]
+y = mesh.points[:,1]
+z = mesh.points[:,2]
+triangles_ind = mesh.cells_dict['triangle']
+num_tris = len(triangles_ind)
 triangles = np.zeros((num_tris, 9), dtype=np.float32)
 
-for i, face in enumerate(elements):
+for i, face in enumerate(triangles_ind):
     assert len(face) == 3
     for d in range(3):
         triangles[i, d * 3 + 0] = x[face[d]]
