e72c62741ed96f8de7fc4f7844d96520fe3ce6d5
aquamin9 <aquamin9@googlemail.com>
2022-03-21 09:14:47 -0700

    fixing memory issues

--- train.py
+++ train.py
@@ -21,10 +21,11 @@ def main ():
 
 
     # start Ray -- add `local_mode=True` here for debugging
-    ray.init(ignore_reinit_error=True)
+    ray.init(ignore_reinit_error=True, include_dashboard=True, logging_level=3)
 
     # register the custom environment
     select_env = "taichi-v0"
+    #select_env = "CartPole-v0"
     #select_env = "fail-v1"
     register_env(select_env, lambda config: Taichi_v0())
     #register_env(select_env, lambda config: Fail_v1())
@@ -33,10 +34,11 @@ def main ():
     # configure the environment and create agent
     config = ppo.DEFAULT_CONFIG.copy()
     config["log_level"] = "WARN"
+    config["num_workers"] = 1
     agent = ppo.PPOTrainer(config, env=select_env)
 
     status = "{:2d} reward {:6.2f}/{:6.2f}/{:6.2f} len {:4.2f} saved {}"
-    n_iter = 5
+    n_iter = 1000
 
     # train a policy with RLlib using PPO
     for n in range(n_iter):
@@ -56,7 +58,7 @@ def main ():
     # examine the trained policy
     policy = agent.get_policy()
     model = policy.model
-    print(model.base_model.summary())
+    #print(model.base_model.summary())
 
 
     # apply the trained policy in a rollout
@@ -65,14 +67,18 @@ def main ():
 
     state = env.reset()
     sum_reward = 0
-    n_step = 20
+    n_step = 2000
 
     for step in range(n_step):
         action = agent.compute_action(state)
         state, reward, done, info = env.step(action)
         sum_reward += reward
-
         env.render()
+
+        if info['cloth'] == True:
+            print("cloth broken")
+            state = env.reset()
+            break
 
         if done == 1:
             # report at the end of each episode
